e424ab311fa1b4cc47fa70a88f8421a6a96e144e
Michael <michaelem3@gmail.com>
2015-08-25 20:45:58 -0400

    Fixed route error

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   get 'users/new'
 
-  root to: 'sessions/new'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
